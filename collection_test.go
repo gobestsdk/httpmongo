24d0df30c29598d383c15a4a4f8cf0d82b87e62a
timeloveboy <734991033@qq.com>
2016-01-16 19:04:04 +0800

    修改完成

--- collection_test.go
+++ collection_test.go
@@ -1,15 +1,22 @@
 package httpmongo
 
 import (
-	"log"
 	"encoding/json"
+	"log"
 	"testing"
+
+	"github.com/golangframework/File"
 )
 
-func Test_One(t *testing.T){
-	var str ="[{\"name\":\"l1\"},{\"name\":\"l2\"}]"
+func Test_One(t *testing.T) {
+	var str = "[{\"name\":\"l1\"},{\"name\":\"l2\"}]"
 	var wo []map[string]string
-	json.Unmarshal([]byte(str),&wo)
+	json.Unmarshal([]byte(str), &wo)
 	log.Print(str)
 	log.Print(wo)
+}
+
+func Test_update(t *testing.T) {
+	var updatestr = File.ReadAllText("./update.json")
+	update("DB")
 }
